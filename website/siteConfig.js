a75d7ec926c530bc1150107dfdfd41172616cfac
Mon Patrick Ganzan <ganzanpatrick.cpe@gmail.com>
2019-03-12 15:47:35 +0800

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Documentation', // Title for your website.
   tagline: 'A website for Fligno documentation',
-  url: 'https://github.com/monpatrickganzan', // Your website URL
+  url: 'https://monpatrickganzan.github.io', // Your website URL
   baseUrl: '/docusaurus', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
